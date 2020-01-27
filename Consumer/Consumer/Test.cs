092d0f74e0b43cc0999ced064beb39746de1a250
Nick <convexarc@outlook.com>
2020-01-27 20:00:37 +0000

    no message

---
+++ Consumer/Consumer/Test.cs
@@ -0,0 +1,13 @@
+﻿using TestPackageSrc;
+
+namespace Consumer
+{
+    public class Test
+    {
+        public void TestPackageOrSource()
+        {
+            var testClass = new TestClass();
+            var result = testClass.TestMethod();
+        }
+    }
+}
